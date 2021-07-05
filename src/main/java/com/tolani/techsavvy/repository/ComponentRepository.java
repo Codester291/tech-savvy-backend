bd242a540d44780866e6846137bac8b1486f2250
Tolani <torhlarny15@gmail.com>
2021-07-05 21:02:45 +0100

    more minor changes

--- src/main/java/com/tolani/techsavvy/repository/ComponentRepository.java
+++ src/main/java/com/tolani/techsavvy/repository/ComponentRepository.java
@@ -1,7 +1,7 @@
-//package com.tolani.techsavvy.repository;
-//
-//import com.tolani.techsavvy.model.Component;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ComponentRepository extends JpaRepository<Component, Long> {
-//}
+package com.tolani.techsavvy.repository;
+
+import com.tolani.techsavvy.model.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComponentRepository extends JpaRepository<Component, Long> {
+}
