1e2a81a53fbd30749dc39ce125349bc7215de65d
Tolani <torhlarny15@gmail.com>
2021-07-05 20:48:29 +0100

    more minor changes

--- src/main/java/com/tolani/techsavvy/model/Component.java
+++ src/main/java/com/tolani/techsavvy/model/Component.java
@@ -1,38 +1,38 @@
-package com.tolani.techsavvy.model;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.io.Serializable;
-import java.time.LocalDate;
-
-@Entity
-@Table(name = "component")
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class Component implements Serializable {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
-    private String ram;
-    private String rom;
-    private String keyboard;
-    private String screen;
-    private String motherboard;
-
-    @OneToOne
-    @JoinColumn(name = "order_id")
-    private Order order;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss")
-    private LocalDate createdAt;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss")
-    private LocalDate updatedAt;
-}
+//package com.tolani.techsavvy.model;
+//
+//import com.fasterxml.jackson.annotation.JsonFormat;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//import java.io.Serializable;
+//import java.time.LocalDate;
+//
+//@Entity
+//@Table(name = "component")
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//public class Component implements Serializable {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private long id;
+//
+//    private String ram;
+//    private String rom;
+//    private String keyboard;
+//    private String screen;
+//    private String motherboard;
+//
+//    @OneToOne
+//    @JoinColumn(name = "order_id")
+//    private Order order;
+//
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss")
+//    private LocalDate createdAt;
+//
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss")
+//    private LocalDate updatedAt;
+//}
--- src/main/java/com/tolani/techsavvy/model/Order.java
+++ src/main/java/com/tolani/techsavvy/model/Order.java
@@ -1,39 +1,39 @@
-package com.tolani.techsavvy.model;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.io.Serializable;
-import java.time.LocalDate;
-
-@Entity
-@Table(name = "order")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Order implements Serializable {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
-    private double amount;
-    private LocalDate deliveryDate;
-
-    @OneToOne(mappedBy = "order")
-    private Component component;
-
-    @JsonManagedReference
-    @ManyToOne
-    private Customer customer;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss")
-    private LocalDate createdAt;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss")
-    private LocalDate updatedAt;
-}
+//package com.tolani.techsavvy.model;
+//
+//import com.fasterxml.jackson.annotation.JsonFormat;
+//import com.fasterxml.jackson.annotation.JsonManagedReference;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//import java.io.Serializable;
+//import java.time.LocalDate;
+//
+//@Entity
+//@Table(name = "order")
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class Order implements Serializable {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private long id;
+//
+//    private double amount;
+//    private LocalDate deliveryDate;
+//
+//    @OneToOne(mappedBy = "order")
+//    private Component component;
+//
+//    @JsonManagedReference
+//    @ManyToOne
+//    private Customer customer;
+//
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss")
+//    private LocalDate createdAt;
+//
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss")
+//    private LocalDate updatedAt;
+//}
