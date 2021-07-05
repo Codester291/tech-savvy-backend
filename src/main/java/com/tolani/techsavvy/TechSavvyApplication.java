c10a4f14b04f4c49c5cd3b9295c49c571b91417d
Tolani <torhlarny15@gmail.com>
2021-07-05 10:38:15 +0100

    first commit :wrench:

---
+++ src/main/java/com/tolani/techsavvy/TechSavvyApplication.java
@@ -0,0 +1,13 @@
+package com.tolani.techsavvy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSavvyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSavvyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tolani/techsavvy/TechSavvyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolani.techsavvy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSavvyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
